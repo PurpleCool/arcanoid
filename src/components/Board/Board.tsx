db208576c3a89df639b1f0d44f8dfcf5a52b0f52
PurpleCool <mariagruzintseva7@gmail.com>
2022-05-21 22:46:14 +0300

    start moving to docker-compose

--- src/components/Board/Board.tsx
+++ src/components/Board/Board.tsx
@@ -1,29 +1,37 @@
-import React, { FC, useEffect, useLayoutEffect, useRef } from 'react'
-import { GAME_ELEMENTS, GAME_ELEMENTS_BBOX } from 'src/const';
-import { ICoords } from '../../types/types'
-import './Board.scss'
+import React, { FC, useEffect, useLayoutEffect, useRef } from "react";
+import { GAME_ELEMENTS, GAME_ELEMENTS_BBOX } from "src/const";
+import { ICoords } from "../../types/types";
+import "./Board.scss";
 
 interface BoardProps {
-	coords: ICoords,
+  coords: ICoords;
 }
 
 const Board: FC<BoardProps> = ({ coords }) => {
-	let boardRef = useRef<SVGRectElement>(null);
+  let boardRef = useRef<SVGRectElement>(null);
 
-	useLayoutEffect(() => {
-		if (boardRef.current) {
-			GAME_ELEMENTS.board = boardRef.current;
-			GAME_ELEMENTS_BBOX.board = boardRef.current.getBBox();
-		}
-	}, [boardRef])
+  useLayoutEffect(() => {
+    if (boardRef.current) {
+      GAME_ELEMENTS.board = boardRef.current;
+      GAME_ELEMENTS_BBOX.board = boardRef.current.getBBox();
+    }
+  }, [boardRef]);
 
+  //TODO разобраться с долгим кликом на стрелку - доска начинает тормозить
+  //TODO при старте игры переводить фокус на поле, чтобы сразу можно было тыкать стрелки
 
-	return (
-		<rect ref={boardRef} className='board' x={coords.x1} y={coords.y1}
-			width={coords.width} height={coords.height}
-			rx={coords.height / 2} ry={coords.height / 2}
-		/>
-	);
-}
+  return (
+    <rect
+      ref={boardRef}
+      className='board'
+      x={coords.x1}
+      y={coords.y1}
+      width={coords.width}
+      height={coords.height}
+      rx={coords.height / 2}
+      ry={coords.height / 2}
+    />
+  );
+};
 
 export default Board;
